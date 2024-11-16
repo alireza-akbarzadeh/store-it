571f6a8a41d16ba8aed6a849f701639a50692def
alierza-akbarzadeh <devtools95@gmail.com>
2024-11-16 18:17:33 +0330

    feat: implement auth layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,17 +1,17 @@
 import type { Metadata } from "next";
 import { Poppins } from "next/font/google";
+
 import "./globals.css";
-import React from "react";
 
 const poppins = Poppins({
   subsets: ["latin"],
-  variable: "--font-poppins",
   weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
+  variable: "--font-poppins",
 });
 
 export const metadata: Metadata = {
-  title: "Store it",
-  description: "The Only storage solution you need.",
+  title: "StoreIt",
+  description: "StoreIt - The only storage solution you need.",
 };
 
 export default function RootLayout({
@@ -21,7 +21,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={`${poppins.variable} font-poppins  antialiased`}>
+      <body className={`${poppins.variable} font-poppins antialiased`}>
         {children}
       </body>
     </html>
