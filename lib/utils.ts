a5de981e38aeeac239b30e165a9250316f2c0d0f
alireza akbarzadeh <devtools95@gmail.com>
2024-12-10 14:25:27 +0330

    feat: ignoreBuildErrors

--- lib/utils.ts
+++ lib/utils.ts
@@ -182,7 +182,6 @@ export const constructDownloadUrl = (bucketFileId: string) => {
   return `${process.env.NEXT_PUBLIC_APPWRITE_ENDPOINT}/storage/buckets/${process.env.NEXT_PUBLIC_APPWRITE_BUCKET}/files/${bucketFileId}/download?project=${process.env.NEXT_PUBLIC_APPWRITE_PROJECT}`;
 };
 
-// DASHBOARD UTILS
 export const getUsageSummary = (totalSpace: any) => {
   return [
     {
